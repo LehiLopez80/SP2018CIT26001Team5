d13ab632ec7877887e5a3e9ac083dfcf1fab4870
Vicky Lopez <Vicky Lopez@LAPTOP-2IA774BG>
2018-05-17 23:14:15 -0500

    17-05-2018 Lesson 4 Assignment completed

---
+++ TheCityOfAaron/src/byui/sp2018cit26001team5/theCityOfAaron/model/ItemType.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.sp2018cit26001team5.theCityOfAaron.model;
+
+/**
+ *
+ * @author Lehi Lopez
+ */
+public enum ItemType {
+    
+    Animal,
+    Tool,
+    Provisions
+    
+}

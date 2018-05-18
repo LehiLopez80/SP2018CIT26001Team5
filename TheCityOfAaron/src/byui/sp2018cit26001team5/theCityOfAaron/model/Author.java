7c9005331d891ff7e7e96b46c15475e76659b9d0
Vicky Lopez <Vicky Lopez@LAPTOP-2IA774BG>
2018-05-17 23:13:56 -0500

    17-05-2018 Lesson 4 Assignment completed

---
+++ TheCityOfAaron/src/byui/sp2018cit26001team5/theCityOfAaron/model/Author.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.sp2018cit26001team5.theCityOfAaron.model;
+
+/**
+ *
+ * @author Lehi Lopez
+ */
+public enum Author {
+    
+    Alonso("Obispo", "Alonso Bernaldo"),
+    Lehi("Ingeniero", "Lehi Lopez"),
+    Salvador("Mister", "Salvador Rubio");
+    
+    private String title;
+    private String name;
+
+    private Author(String title, String name) {
+        this.title = title;
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" + "title=" + title + ", name=" + name + '}';
+    }
+   
+    
+}

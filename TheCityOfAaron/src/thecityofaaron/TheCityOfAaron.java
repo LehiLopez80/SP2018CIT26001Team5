fd8f37a6a23348187c3bf4c937d2cf06f112d52d
Vicky Lopez <Vicky Lopez@LAPTOP-2IA774BG>
2018-05-17 23:08:32 -0500

    17-05-2018 Lesson 4 Assignment completed

--- TheCityOfAaron/src/thecityofaaron/TheCityOfAaron.java
+++ TheCityOfAaron/src/thecityofaaron/TheCityOfAaron.java
@@ -5,9 +5,15 @@
  */
 package thecityofaaron;
 
+import byui.sp2018cit26001team5.theCityOfAaron.model.Author;
+import byui.sp2018cit26001team5.theCityOfAaron.model.Game;
+import byui.sp2018cit26001team5.theCityOfAaron.model.Player;
+import byui.sp2018cit26001team5.theCityOfAaron.model.Storehouse;
+import java.util.ArrayList;
+
 /**
  *
- * @author Vicky Lopez
+ * @author Lehi Lopez
  */
 public class TheCityOfAaron {
 
@@ -15,7 +21,57 @@ public class TheCityOfAaron {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //create objects
+        Player playerOne = new Player();
+        Game gameOne = new Game();
+        ArrayList<Game> games = new ArrayList<Game>();
+        Storehouse storehouseOne = new Storehouse();
+        
+        //call setter functions
+        
+        playerOne.setName("Alonso");
+        playerOne.setGames(games);
+             
+        gameOne.setCurrentPopulation(100);
+        gameOne.setAcresOwned(1000);
+        gameOne.setWheatInStorage(3000);
+        gameOne.setCurrentYear(1);
+        gameOne.setPlayer(playerOne);
+        gameOne.setStorehouse(storehouseOne);
+        
+        Game gameTwo = new Game();
+        storehouseOne.setGame(gameTwo);
+        
+        //call getter functions and print to screen
+        
+        String playerOneName = playerOne.getName();
+        System.out.println("Name = " + playerOneName);
+        System.out.println(playerOne.toString());
+        
+        int gameOneCurrentPopulation = gameOne.getCurrentPopulation();
+        System.out.println("Current Population = " + gameOneCurrentPopulation);
+        int gameOneAcresOwned = gameOne.getAcresOwned();
+        System.out.println("Acres Owned = " + gameOneAcresOwned);
+        int gameOneWheatInStorage = gameOne.getWheatInStorage();
+        System.out.println("Wheat In Storage = " + gameOneWheatInStorage);
+        int gameOneCurrentYear = gameOne.getCurrentYear();
+        System.out.println("Current Year = " + gameOneCurrentYear);
+        System.out.println(gameOne.toString());
+        
+        System.out.println(storehouseOne.toString());
+        
+        //test the enum
+        String authorOneTitle = Author.Alonso.getTitle();
+        String authorOneName = Author.Alonso.getName();
+        System.out.println("Author One = " + authorOneTitle + " " + authorOneName);
+        String authorTwoTitle = Author.Lehi.getTitle();
+        String authorTwoName = Author.Lehi.getName();
+        System.out.println("Author Two = " + authorTwoTitle + " " + authorTwoName);
+        String authorThreeTitle = Author.Salvador.getTitle();
+        String authorThreeName = Author.Salvador.getName();
+        System.out.println("Author Three = " + authorThreeTitle + " " + authorThreeName);
+        
     }
     
 }

3fafecbaccbb63b12262b292e3171d40fd019a47
familia <familia@192.168.0.14>
2018-06-12 23:16:05 -0500

    12-jun-18_Individual Assignment Week 8. Game Menu

---
+++ TheCityOfAaron/src/byui/sp2018cit26001team5/theCityOfAaron/view/GameMenu.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.sp2018cit26001team5.theCityOfAaron.view;
+import java.util.Scanner;
+/**
+ *
+ * @author Salvador Rubio
+ */
+public class GameMenu {
+    
+     private static final String MENU = "\n"
+             
+             + "\n----------------------------------"
+                + "\n Game Menu                        "
+                + "\n ---------------------------------"
+                + "\n1 - View the map"
+                + "\n2 - Move to a new location"
+                + "\n3 - Manage the crops"
+                + "\n4 - Live the year"
+                + "\n5 - Reports menu"
+                + "\n6 - Save game"
+                + "\n7 - Return to the main menu"
+                + "\n----------------------------------";
+           
+    public void displayGameMenu() {
+        
+        boolean done = false; //set flag to not done
+        
+        do {
+            //promp for and gets players name
+            String menuOption = this.getMenuOption();
+            
+            
+            done = this.doAction(menuOption);
+        } while (!done);
+    }
+    
+     private String getMenuOption() {      
+        
+        String value = ""; //value to be returned
+        boolean valid;  //initialize to not valid
+        valid = false;
+         
+        System.out.println(MENU);
+        
+        while (!valid) { // loop while an invalid value is enter
+            //System.out.println("\n" + this.promptMessage);
+            
+            Scanner keyboard = new Scanner(System.in); //get infile for keyboard
+            
+            
+            value = keyboard.nextLine();//getnext line typed on keyboard
+            //value = value.trim(); //trim off leading and trailing blanks
+            //System.out.println("Trimmed: "+ value);
+                    
+            
+            if (value.length() < 1 ){ //if value is blank
+                System.out.println("\nInvalid value: value cannot be blank");
+                continue;
+                            }
+            break;
+        }
+        return value; //returns the value 
+        }
+     
+      private boolean doAction(String menuOption) {
+       menuOption = menuOption.toUpperCase(); //converts to upper case
+       
+       switch (menuOption){
+           case "1": // view the map
+               this.viewTheMap();
+               break;
+               
+           case "2": //Move to a new location
+               this.moveNewLocation();
+               break;
+               
+           case "3": // Manage the crops
+               this.manageTheCrops();
+               break;
+            
+           case "4": //Live the year
+               this.liveTheYear();
+               break;
+               
+           case "5": //Reports menu
+               this.reports();
+               break;
+               
+           case "6": //Save game
+               this.saveGame();
+               break;
+               
+            case "7": //Return to the main menu
+               this.returnMainMenu();
+               break;   
+              
+           default:
+               System.out.println("\nInvalid selection *** Try again");
+               break;     
+       }
+       return false;
+        }
+      
+    private void viewTheMap() {
+       
+       System.out.println("*** Would you like to view the map?");
+    }
+
+    private void moveNewLocation() {
+        System.out.println("*** The new location is..."); 
+    }
+
+    private void manageTheCrops() {
+        System.out.println("*** Would you like to manage the crops?"); 
+    }
+
+    private void liveTheYear() {
+        System.out.println("Chose this option to live the year."); 
+    }
+
+    private void reports() {
+        System.out.println("Get the report of the game."); 
+    }   
+    
+    private void saveGame() {
+        System.out.println("Here you can save the game."); 
+    }
+    
+    private void returnMainMenu() {
+        System.out.println("Return to the main menu."); 
+    }
+    
+}

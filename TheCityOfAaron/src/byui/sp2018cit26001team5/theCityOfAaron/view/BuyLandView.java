6e6b9c18fe2c46398e385b9a3a42aa9089a94d35
familia <familia@192.168.0.14>
2018-06-13 23:23:54 -0500

    12-jun-18_Individual Assignment Week 8. Game Menu View Buy Land View

--- TheCityOfAaron/src/byui/sp2018cit26001team5/theCityOfAaron/view/BuyLandView.java
+++ TheCityOfAaron/src/byui/sp2018cit26001team5/theCityOfAaron/view/BuyLandView.java
@@ -17,12 +17,58 @@ public class BuyLandView {
     public BuyLandView() {
     }
     
-    GameControl gameControl = new GameControl();
+    public void displayBuyLandView() {
+        
+        System.out.println("Buy Land view");
+        
+        GameControl gameControl = new GameControl();
     
-    int randomInt = (int) ((int)11*Math.random());
-    int acresPrice = GameControl.calculateAcresPrice (randomInt);
-    // System.out.println("The price of the land is: " + acresPrice);
-    
-    // System.out.println("You must enter a value");
+        int randomInt = (int) ((int)11*Math.random());
+        int acresPrice = GameControl.calculateAcresPrice (randomInt);
+        
+        System.out.println("The price of the land is: " + acresPrice);
+        System.out.println("\nHow many acres do you whish to buy?");
+        
+        boolean endView = false;
+        do {
+            int input = this.getInput();
+            endView = this.doAction(input);
+                  
+        } while(endView != true);        
+    }
+
+    private int getInput() {
+        
+        int input = 0;
+        
+        boolean valid = false;
+        
+        while(valid == false){
+            Scanner inFile;
+            inFile = new Scanner(System.in);
+
+            input = inFile.nextInt();
+                        
+            if(input < 0){
+                System.out.println("You must not enter a negative value");
+                continue;
+            }
+            
+            if(input > 3000){
+                System.out.println("You don't own that many acres");
+                continue;
+            }
+            
+            valid = true;
+        }
+        return input;        
+    }
+
+    private boolean doAction(int input) {
+        
+        System.out.println("Update Acres owned and bushels in store");
+        return true;
+    }
+  
     
 }
--- TheCityOfAaron/src/byui/sp2018cit26001team5/theCityOfAaron/view/ReportsMenuView.java
+++ TheCityOfAaron/src/byui/sp2018cit26001team5/theCityOfAaron/view/ReportsMenuView.java
@@ -2,20 +2,22 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- 
+ */
 package byui.sp2018cit26001team5.theCityOfAaron.view;
+
 import java.util.Scanner;
-*/
+
 /**
  *
  * @author Salvador Rubio
+ */
  
 public class ReportsMenuView {
     
      private static final String MENU = "\n"
              
-             + "\n----------------------------------"
-                + "\n Game Menu                        "
+                + "\n----------------------------------"
+                + "\n Reports Menu                        "
                 + "\n ---------------------------------"
                 + "\n1 - View the animals in the storehouse"
                 + "\n2 - View the tools in the storehouse"
@@ -37,6 +39,85 @@ public class ReportsMenuView {
         } while (!done);
     }
      
+     private String getMenuOption() {      
+        
+        String value = ""; //value to be returned
+        boolean valid;  //initialize to not valid
+        valid = false;
+         
+        System.out.println(MENU);
+        
+        while (!valid) { // loop while an invalid value is enter
+            //System.out.println("\n" + this.promptMessage);
+            
+            Scanner keyboard = new Scanner(System.in); //get infile for keyboard
+            
+            
+            value = keyboard.nextLine();//getnext line typed on keyboard
+            //value = value.trim(); //trim off leading and trailing blanks
+            //System.out.println("Trimmed: "+ value);
+                    
+            
+            if (value.length() < 1 ){ //if value is blank
+                System.out.println("\nInvalid value: value cannot be blank");
+                continue;
+                            }
+            break;
+        }
+        return value; //returns the value 
+        }
+     
+     private boolean doAction(String menuOption) {
+       menuOption = menuOption.toUpperCase(); //converts to upper case
+       
+       switch (menuOption){
+           case "1": // view the map
+               this.viewAnimalsInStorehouse();
+               break;
+               
+           case "2": //Move to a new location
+               this.viewToolsInStorehouse();
+               break;
+               
+           case "3": // Manage the crops
+               this.viewProvisionsInStorehouse();
+               break;
+            
+           case "4": //Live the year
+               this.viewAuthorsOfThisGame();
+               break;               
+                          
+            case "5": //Return to the main menu
+               this.returnMainMenu();
+               return true;   
+              
+           default:
+               System.out.println("\nInvalid selection *** Try again");
+               break;     
+       }
+       return false;
+        }
+     
+     private void viewAnimalsInStorehouse() {
+       
+       System.out.println("*** Here you can check the animals");
+    }
+
+    private void viewToolsInStorehouse() {
+        System.out.println("*** Here you can check the tools"); 
+    }
+
+    private void viewProvisionsInStorehouse() {
+        System.out.println("*** Here you can check the provisions"); 
+    }
+
+    private void viewAuthorsOfThisGame() {
+        System.out.println("Here you can check the authors of the game"); 
+    }
+        
+    private void returnMainMenu() {
+        System.out.println("Return to the main menu."); 
+    }
+     
     
 }
-*/
